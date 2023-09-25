2df3520ff3c55d0c415a445cffa19f896c03619e
dainnin <36818514+dainnin@users.noreply.github.com>
2023-09-25 17:17:36 -0300

    Add files via upload

--- logica/principal.js
+++ logica/principal.js
@@ -25,43 +25,55 @@ llamarObjeto().then((obJSON) => {
                      articulo.setAttribute('class','card')
 
    articulo.innerHTML=`
-   <picture><img class='card-img-top' src="${imgSrc}" alt=""></picture>
+   <picture><img class='card-img-top' src="${imgSrc[0]}" alt=""></picture>
    <div class='card-body'>
-    <h4 class='card-title'>${Object.keys(obJSON.oradores)[i]}</h4>
-    <p class='card-text'>Descripcion del contenido</p>
+    <h4 class='card-title'>${imgSrc[1]}</h4>
+    <p class='card-text'>${imgSrc[2]}</p>
     <a class='btn btn-primary' href="">Boton Bootstrap 5</a>
  </div> `
 document.getElementById('oradores').appendChild(articulo)
    }//Fin de Card Oradores
 
+   //Logo Header
     document.getElementsByTagName('header')[0].getElementsByTagName('img')[0].src =  Object.values(obJSON.logoNav)
+    const menHeader = document.createElement('menu')
+    menHeader.innerHTML=`
+    <a href="">La conferencia</a>
+    <a href="">Los oradores</a>
+    <a href="">El lugar y fecha</a>
+    <a href="">Conviértete en orador</a>
+    <a href="">Comprar tickets</a>
+    
+    `
+    document.getElementsByTagName('header')[0].appendChild(menHeader)
    
-  /*   banners.style.backgroundImage=`url(${Object.values(obJSON.banner)[0]})`*/
-   banners.style.height='calc(70vw / 16 * 9)'
-   /* banners.style.backgroundSize='contain'
-   banners.style.backgroundRepeat='no-repeat' */
-   const descr=document.getElementById('descript')
-   descr.getElementsByTagName('img')[0].src=`${Object.values(obJSON.descript)[0]}`
-}//Fin obJSON
-
-)//Fin funcion
-const menHeader = document.createElement('menu')
-menHeader.innerHTML=`
-<a href="">La conferencia</a>
-<a href="">Los oradores</a>
-<a href="">El lugar y fecha</a>
-<a href="">Conviértete en orador</a>
-<a href="">Comprar tickets</a>
-
-`
-document.getElementsByTagName('header')[0].appendChild(menHeader)
-const elemBanner=document.createElement('div')
+    //Fin
+   
+   
+    //Creacion del banner
+    const elemBanner=document.createElement('div')
 elemBanner.innerHTML=`
 <h1>Conf Bs As</h1>
 <p><strong>Bs As llega por primera vez a Argentina. Un evento para compartir con nuestra comunidad el
  conocimiento y expreriencia delos expertos que estan creando el futuro de Internet. Ven a 
  conocer a mienmbros del evento, a otros estudiantes de Codo a Codo y los oradores del primer 
  nivel que tenemos para. Te esperamos</strong></p>
-<a href="">Quiero ser orador</a><a href="">Comprar tickets</a>
+<a class="btn" href="">Quiero ser orador</a>
+<a class="btn btn-primary" href="">Comprar tickets</a>
 `
-banners.appendChild(elemBanner)
+banners.appendChild(elemBanner)        
+   //Fin
+
+   //Estructura de Datos extra
+   const descr=document.getElementById('descript')
+   descr.getElementsByTagName('img')[0].src=`${Object.values(obJSON.descript)[0]}`
+   
+   //Fin
+
+
+}//Fin obJSON
+
+)//Fin funcion
+
+
+
